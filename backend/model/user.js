70dcde4ccb42d8f7825ec301ca0429a2ba83bd78
Chukwuemeka Momodu <cmomodu@uwo.ca>
2022-11-24 11:39:51 -0500

    backend - added user entity
    created the user entity for database

---
+++ backend/model/user.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema(
+    {
+        email: {
+            type: String,
+            index: {unique: true}
+        },
+        password: {
+            type: String,
+        },
+        firstname: {
+            type: String,
+        },
+        lastname: {
+            type: String,
+        },
+        role: {
+            type: String,
+        },
+        email_verified_flag: {
+            type: String,
+        },
+        type: {
+            type: String,
+        },
+        status: {
+            type: String,
+        },
+    }
+);
+
+module.exports = mongoose.model('User', userSchema);
