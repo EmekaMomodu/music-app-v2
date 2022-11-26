d14fc7b2a721d138d262b8c0f1e7199608c2871d
Chukwuemeka Momodu <cmomodu@uwo.ca>
2022-11-25 20:55:14 -0500

    added config file, added server.js to handle server logic

--- backend/app.js
+++ backend/app.js
@@ -8,8 +8,6 @@ const trackRoutes = require('./route/track');
 const playlistRoutes = require('./route/playlist');
 
 const app = express();
-const port = 3001;
-const databaseConnectionUrl = 'mongodb+srv://ece9065:ece9065@cluster0.zry6vyf.mongodb.net/?retryWrites=true&w=majority';
 const pathPrefix = '/api';
 
 app.use(bodyParser.json());
@@ -29,6 +27,7 @@ app.use(`${pathPrefix}/tracks`, trackRoutes);
 
 app.use(`${pathPrefix}/playlists`, playlistRoutes);
 
+// Error handling middleware
 app.use((error, req, res, next) => {
     console.log(error);
     const status = error.statusCode || 500;
@@ -37,11 +36,4 @@ app.use((error, req, res, next) => {
     res.status(status).json(response);
 });
 
-mongoose.connect(databaseConnectionUrl)
-    .then(result => {
-        app.listen(port, () => {
-            console.log(`App listening on port ${port}`)
-        });
-    })
-    .catch(err => console.log(err));
-
+module.exports = app;
--- backend/model/playlist.js
+++ backend/model/playlist.js
@@ -40,9 +40,20 @@ const playlistSchema = new Schema(
                 track_duration: {
                     type: String,
                 },
-                track_genres: {
-                    type: String,
-                },
+                track_genres: [
+                    {
+                        genre_id: {
+                            type: Number
+                        },
+                        genre_title: {
+                            type: String,
+                            index: true
+                        },
+                        genre_url: {
+                            type: String
+                        }
+                    }
+                ],
                 track_number: {
                     type: Number,
                 },
