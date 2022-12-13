d48b21b06fa7fc991c92c17184330e175f2fe45d
AmirGAG93 <agazeran@uwo.ca>
2022-12-13 14:48:05 -0500

    modularized login, landing and register component

---
+++ frontend/src/app/views/welcome/landing/landing.component.ts
@@ -0,0 +1,17 @@
+import {Component} from '@angular/core';
+import { faRightToBracket } from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+    selector: 'app-landing',
+    templateUrl: './landing.component.html',
+    styleUrls: ['./landing.component.scss']
+})
+export class LandingComponent {
+
+    faRightToBracket = faRightToBracket;
+    isLoggedIn: boolean = false;
+
+    constructor() {
+    }
+
+}
